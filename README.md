8b4f5a3581763d86438bc7134fbf1a6d4824d0c2
Giacomo Mr. Wolf Furlan <giacomo.furlan@fastwebnet.it>
2018-11-13 00:53:55 +0100

    Added Debian Stretch builds

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# Docker go with extras
+# Docker go with extras [![Build Status](https://travis-ci.org/MicroLayers/docker-go-with-extras.svg?branch=master)](https://travis-ci.org/MicroLayers/docker-go-with-extras)
+
 A go docker image based on the official golang one with extras to build go projects.
+
+Based on both **Alpine** and **Debian Stretch** base images.
 
 It includes:
 
--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-oneMonthAgo=`date -d "-1 month" +%Y-%m-01`
-oneMonthAgoTimestamp=`date -d $oneMonthAgo +%s`
+oneMonthAgo=$(date -d "-1 month" +%Y-%m-01)
+oneMonthAgoTimestamp=$(date -d "$oneMonthAgo" +%s)
 
 baseUrl="https://registry.hub.docker.com/v2/repositories/library/golang/tags/?page="
 
 processResult() {
   local result=$1
 
-  version=`echo $result | grep alpine | sed -e 's/"//g'`
+  version=$(echo "$result" | grep -P '(\-alpine|\-stretch)' | sed -e 's/"//g')
 
   if [ "$version" == "" ]; then
     return 0
@@ -16,40 +16,43 @@ processResult() {
 
   echo "Processing version: $version"
 
-  dockerfileName="Dockerfile.$version.yml"
-  sed "s/{VERSION}/$version/" Dockerfile.template.yml > $dockerfileName
+  templateKind=$(echo "$version" | grep -Po "(alpine|stretch)")
+  templateFileName="Dockerfile.template.$templateKind"
+  dockerfileName="Dockerfile.$version"
+  sed "s/{VERSION}/$version/" "Dockerfile.template.$templateKind" > "$dockerfileName"
 
-  docker build -f $dockerfileName -t microlayers/golang-with-extras:$version .
-  docker push microlayers/golang-with-extras:$version
-  rm $dockerfileName
+  docker build -f "$dockerfileName" -t "microlayers/golang-with-extras:$version" .
+  rm "$dockerfileName"
+
+  docker push "microlayers/golang-with-extras:$version" &
 
   return 0
 }
 
 page=0
-while [ true ]; do
+while true ; do
   page=$((page + 1))
-  data=`curl -s $baseUrl$page`
-  next=`echo $data|jq .next`
+  data=$(curl -s "$baseUrl$page")
+  next=$(echo "$data"|jq .next)
   index=0
 
-  result=`echo $data|jq .results[$index].name`
-  lastUpdated=`echo $data|jq .results[$index].last_updated | grep -Poe "\d{4}-\d{2}-\d{2}"`
+  result=$(echo "$data"|jq .results[$index].name)
+  lastUpdated=$(echo "$data"|jq .results[$index].last_updated | grep -Poe "\d{4}-\d{2}-\d{2}")
   while [ "$result" != "null" ]; do
-    timestamp=`date -d $lastUpdated +%s`
-    if [ $oneMonthAgoTimestamp -gt $timestamp ]; then
+    timestamp=$(date -d "$lastUpdated" +%s)
+    if [ "$oneMonthAgoTimestamp" -gt "$timestamp" ]; then
       exit 0
     fi
 
-    processResult $result
+    processResult "$result"
 
     index=$((index + 1))
-    result=`echo $data|jq .results[$index].name`
-    lastUpdated=`echo $data|jq .results[$index].last_updated | grep -Poe "\d{4}-\d{2}-\d{2}"`
+    result=$(echo "$data"|jq .results[$index].name)
+    lastUpdated=$(echo "$data"|jq .results[$index].last_updated | grep -Poe "\d{4}-\d{2}-\d{2}")
   done
 
   if [ "$next" == "null" ] || [ $index -gt 10 ]; then
     break
   fi
 done
-
+wait
